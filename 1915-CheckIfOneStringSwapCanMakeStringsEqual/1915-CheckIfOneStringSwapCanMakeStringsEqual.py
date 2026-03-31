b67f3a5d83ee1452e23625c8df40c082183532d8
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:33:40 +0530

    Create 1915-CheckIfOneStringSwapCanMakeStringsEqual

---
+++ 1915-CheckIfOneStringSwapCanMakeStringsEqual/1915-CheckIfOneStringSwapCanMakeStringsEqual.py
@@ -0,0 +1,18 @@
+# Last updated: 3/31/2026, 9:33:39 PM
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        i=-1
+        j=-1
+        cnt=0
+        for k in range(0, len(s1)):
+            if s1[k]!=s2[k]:
+                cnt+=1
+                if i==-1: i=k
+                elif j==-1: j=k
+        
+        if cnt==0: 
+            return True
+        elif cnt==2 and s1[i]==s2[j] and s1[j]==s2[i]: 
+            return True
+
+        return False
