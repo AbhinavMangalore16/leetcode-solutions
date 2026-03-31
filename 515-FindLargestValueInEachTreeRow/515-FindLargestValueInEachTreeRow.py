2fefcd03e8ec97ab5cb5973863c9372cbb57d6b2
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:37:44 +0530

    Create 515-FindLargestValueInEachTreeRow

---
+++ 515-FindLargestValueInEachTreeRow/515-FindLargestValueInEachTreeRow.py
@@ -0,0 +1,24 @@
+# Last updated: 3/31/2026, 9:37:43 PM
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        ans = []
+        queue = deque([root])
+        
+        while queue:
+            current_length = len(queue)
+            curr_max = float("-inf")
+            
+            for _ in range(current_length):
+                node = queue.popleft()
+                curr_max = max(curr_max, node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            ans.append(curr_max)
+        
+        return ans
