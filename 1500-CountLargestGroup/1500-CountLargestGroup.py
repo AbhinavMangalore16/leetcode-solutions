a512434df4b18bc72453328df33bbd83f68d9e84
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:34:41 +0530

    Create 1500-CountLargestGroup

---
+++ 1500-CountLargestGroup/1500-CountLargestGroup.py
@@ -0,0 +1,14 @@
+# Last updated: 3/31/2026, 9:34:40 PM
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        from collections import defaultdict
+
+        digit_count = defaultdict(int)
+        max_size = 0
+
+        for i in range(1, n + 1):
+            s = sum(map(int, str(i)))
+            digit_count[s] += 1
+            max_size = max(max_size, digit_count[s])
+
+        return sum(1 for count in digit_count.values() if count == max_size)
