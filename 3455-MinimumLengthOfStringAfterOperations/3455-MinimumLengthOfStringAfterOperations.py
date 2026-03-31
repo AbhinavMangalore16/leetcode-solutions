5a9ac5d52bd7165d923dd0fff691ac8709e2c223
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:29:13 +0530

    Create 3455-MinimumLengthOfStringAfterOperations

---
+++ 3455-MinimumLengthOfStringAfterOperations/3455-MinimumLengthOfStringAfterOperations.py
@@ -0,0 +1,15 @@
+# Last updated: 3/31/2026, 9:29:11 PM
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        char_frequency = [0] * 26
+        total_length = 0
+        for char in s:
+            char_frequency[ord(char) - ord('a')] += 1
+        for frequency in char_frequency:
+            if frequency == 0:
+                continue
+            if frequency % 2 == 0:
+                total_length += 2
+            else:
+                total_length += 1
+        return total_length 
