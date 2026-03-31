8a5e59959eca8aee12687f19faabc726b646ffe1
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:32:23 +0530

    Create 2280-CountGoodTripletsInAnArray

---
+++ 2280-CountGoodTripletsInAnArray/2280-CountGoodTripletsInAnArray.py
@@ -0,0 +1,16 @@
+# Last updated: 3/31/2026, 9:32:21 PM
+class Solution:
+    def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        mpp = {}
+        for i, v in enumerate(nums1):
+            mpp[v] = i
+        n = len(nums2)
+        total = 0
+        st = []
+        for x in nums2:
+            idx = mpp[x]
+            left = bisect.bisect_left(st, idx)
+            right = (n - 1 - idx) - (len(st) - left)
+            total += left * right
+            bisect.insort(st, idx)
+        return total
