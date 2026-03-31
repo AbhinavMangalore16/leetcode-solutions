9e0a1ae23c08a4d0e620842d3f9057dd388ff108
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:35:32 +0530

    Create 1250-LongestCommonSubsequence

---
+++ 1250-LongestCommonSubsequence/1250-LongestCommonSubsequence.py
@@ -0,0 +1,14 @@
+# Last updated: 3/31/2026, 9:35:30 PM
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [0] * len(text1)
+        longest = 0
+        for c in text2:
+            cur = 0
+            for i, val in enumerate(dp):
+                if cur < val:
+                    cur = val
+                elif c == text1[i]:
+                    dp[i] = cur + 1
+                    longest = max(longest, cur + 1)
+        return longest
