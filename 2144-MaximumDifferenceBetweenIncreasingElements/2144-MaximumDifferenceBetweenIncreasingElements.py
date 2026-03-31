cb7e1aad78797d30196240f55d0aeca551560db0
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:33:04 +0530

    Create 2144-MaximumDifferenceBetweenIncreasingElements

---
+++ 2144-MaximumDifferenceBetweenIncreasingElements/2144-MaximumDifferenceBetweenIncreasingElements.py
@@ -0,0 +1,9 @@
+# Last updated: 3/31/2026, 9:33:02 PM
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        xMin, n=10**9, len(nums)
+        maxD=-1
+        for x in nums:
+            if x<=xMin: xMin=x
+            else: maxD=max(maxD, x-xMin)
+        return maxD
