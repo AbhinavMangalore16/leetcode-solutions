2c33fed2c4220fe0ff202ca0f11d63b644e9b306
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:32:28 +0530

    Create 2262-SolvingQuestionsWithBrainpower

---
+++ 2262-SolvingQuestionsWithBrainpower/2262-SolvingQuestionsWithBrainpower.py
@@ -0,0 +1,13 @@
+# Last updated: 3/31/2026, 9:32:27 PM
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        dp = [0] * len(questions)
+        for i in range(len(questions) - 1, -1, -1):
+            index = i + questions[i][1] + 1
+            if index < len(questions):
+                dp[i] = dp[index] + questions[i][0]
+            else:
+                dp[i] = questions[i][0]
+            if i < len(questions) - 1:
+                dp[i] = max(dp[i + 1], dp[i])
+        return dp[0]
