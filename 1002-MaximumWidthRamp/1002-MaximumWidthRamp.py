a39e28c5a2564380285b80b410737ab660629829
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:36:05 +0530

    Create 1002-MaximumWidthRamp

---
+++ 1002-MaximumWidthRamp/1002-MaximumWidthRamp.py
@@ -0,0 +1,22 @@
+# Last updated: 3/31/2026, 9:36:03 PM
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        ans = 0
+        n = len(nums)
+
+        # Create a list of tuples (element, index)
+        vp = [(nums[i], i) for i in range(n)]
+
+        # Sort the list based on the element values
+        vp.sort()
+
+        # Keep track of the minimum index seen so far
+        min_index = vp[0][1]
+
+        # Traverse the sorted list to calculate the maximum width ramp
+        for i in range(1, n):
+            current_index = vp[i][1]
+            ans = max(ans, current_index - min_index)
+            min_index = min(min_index, current_index)
+
+        return ans
