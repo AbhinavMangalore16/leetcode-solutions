97665ddb47f65c49bd48b7339a2efbf7f949dca2
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:30:51 +0530

    Create 2690-HouseRobberIv

---
+++ 2690-HouseRobberIv/2690-HouseRobberIv.py
@@ -0,0 +1,25 @@
+# Last updated: 3/31/2026, 9:30:49 PM
+class Solution:
+    def canRob(self, nums, mid, k):
+        count = 0
+        n = len(nums)
+        i = 0
+        while i < n:
+            if nums[i] <= mid:
+                count += 1
+                i += 1
+            i += 1
+        return count >= k
+
+    def minCapability(self, nums, k):
+        left, right = 1, max(nums)
+        ans = right
+        while left <= right:
+            mid = (left + right) // 2
+            if self.canRob(nums, mid, k):
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
+        
