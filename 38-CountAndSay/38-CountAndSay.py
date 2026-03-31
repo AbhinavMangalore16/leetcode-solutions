3cdc16f8c046911c1d30bf3a0d652f85a81ccb10
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:40:53 +0530

    Create 38-CountAndSay

---
+++ 38-CountAndSay/38-CountAndSay.py
@@ -0,0 +1,22 @@
+# Last updated: 3/31/2026, 9:40:52 PM
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        curr = "1"
+        if n == 1:
+            return curr
+        for _ in range(2, n + 1):
+            sb = []
+            cnt = 1
+            ele = curr[0]
+            for j in range(1, len(curr)):
+                if curr[j] == ele:
+                    cnt += 1
+                else:
+                    sb.append(str(cnt))
+                    sb.append(ele)
+                    ele = curr[j]
+                    cnt = 1
+            sb.append(str(cnt))
+            sb.append(ele)
+            curr = ''.join(sb)
+        return curr
