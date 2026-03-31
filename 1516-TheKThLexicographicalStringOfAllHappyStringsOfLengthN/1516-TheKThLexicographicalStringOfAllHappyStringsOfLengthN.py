a17d64a55ae4378e5707f80e5eafcd8ce648977f
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:34:39 +0530

    Create 1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN

---
+++ 1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN/1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN.py
@@ -0,0 +1,20 @@
+# Last updated: 3/31/2026, 9:34:37 PM
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        if k > (3 << (n - 1)):  
+            return ""  
+        
+        queue = deque([""])  
+        
+        while k:
+            curr = queue.popleft()
+            
+            for c in "abc":
+                if not curr or curr[-1] != c:
+                    queue.append(curr + c)
+                    if len(curr) + 1 == n:
+                        k -= 1
+                if k == 0:
+                    break
+        
+        return queue[-1]
