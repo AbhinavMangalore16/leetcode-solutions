408a10612438c7789d3f058ac10a39bdc6235347
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:28:39 +0530

    Create 3773-MinimumPairRemovalToSortArrayI

---
+++ 3773-MinimumPairRemovalToSortArrayI/3773-MinimumPairRemovalToSortArrayI.cpp
@@ -0,0 +1,27 @@
+// Last updated: 3/31/2026, 9:28:38 PM
+class Solution {
+public:
+    int minPair(vector<int> v){
+        int minSum = 1e9;
+        int pos = -1;
+        for(int i = 0; i < (int)v.size() - 1; i ++){
+            if(v[i] + v[i + 1] < minSum){
+                minSum = v[i] + v[i + 1];
+                pos = i;
+            }
+        }
+        return pos;
+    }
+    void mergePair(vector<int> &v, int pos){
+        v[pos] += v[pos + 1];
+        v.erase(v.begin() + pos + 1);
+    }
+    int minimumPairRemoval(vector<int>& nums) {
+        int ops = 0;
+        while(!is_sorted(nums.begin(), nums.end())){
+            mergePair(nums, minPair(nums));
+            ops += 1;
+        }
+        return ops;
+    }
+};
