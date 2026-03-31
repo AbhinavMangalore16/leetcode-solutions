9a80d884c5aea5d6265ce68841342ac736679d6d
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:36:14 +0530

    Create 966-BinarySubarraysWithSum

---
+++ 966-BinarySubarraysWithSum/966-BinarySubarraysWithSum.py
@@ -0,0 +1,12 @@
+# Last updated: 3/31/2026, 9:36:12 PM
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        c = 0
+        prefix = [0]
+        for num in nums:
+            prefix.append(prefix[-1]+num)
+        d = {}
+        for s in prefix:
+            c += d.get(s-goal, 0) 
+            d[s] = d.get(s,0)+ 1
+        return c
