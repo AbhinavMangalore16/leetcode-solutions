9f7002915ee5612e264f5fa2d80df7ae7e6fb3b7
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:30:02 +0530

    Create 3195-SeparateBlackAndWhiteBalls

---
+++ 3195-SeparateBlackAndWhiteBalls/3195-SeparateBlackAndWhiteBalls.py
@@ -0,0 +1,13 @@
+# Last updated: 3/31/2026, 9:30:01 PM
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        white=0
+        res=0
+        j=0
+        n=len(s)
+        while(j<n):
+            if s[j]=='0':
+                res+=white
+            white+=1 if s[j]=='1' else 0
+            j+=1 
+        return res
