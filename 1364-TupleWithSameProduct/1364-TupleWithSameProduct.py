6eda099c8ede719652fa00867186725dd167aa36
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:35:16 +0530

    Create 1364-TupleWithSameProduct

---
+++ 1364-TupleWithSameProduct/1364-TupleWithSameProduct.py
@@ -0,0 +1,13 @@
+# Last updated: 3/31/2026, 9:35:15 PM
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        ans = 0
+        count = collections.Counter()
+
+        for i in range(len(nums)):
+            for j in range(i):
+                prod = nums[i] * nums[j]
+                ans += count[prod] * 8
+                count[prod] += 1
+
+        return ans
