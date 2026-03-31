e022037c9315683c89c85f8d7e4d3f0d9ec63b55
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:36:10 +0530

    Create 982-MinimumIncrementToMakeArrayUnique

---
+++ 982-MinimumIncrementToMakeArrayUnique/982-MinimumIncrementToMakeArrayUnique.py
@@ -0,0 +1,12 @@
+# Last updated: 3/31/2026, 9:36:08 PM
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+        numTracker = 0
+        minIncreament = 0
+
+        for num in nums:
+            numTracker = max(numTracker, num)
+            minIncreament += numTracker - num
+            numTracker += 1
+        return minIncreament
