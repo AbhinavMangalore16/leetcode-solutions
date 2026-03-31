334171e1824a250cb95f0e7773899f0f8704e592
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:28:59 +0530

    Create 3605-ConstructTheMinimumBitwiseArrayI

---
+++ 3605-ConstructTheMinimumBitwiseArrayI/3605-ConstructTheMinimumBitwiseArrayI.py
@@ -0,0 +1,17 @@
+# Last updated: 3/31/2026, 9:28:57 PM
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for num in nums:
+            is_found = False
+            for i in range(num):
+                if i | (i + 1) == num:
+                    ans.append(i)
+                    is_found = True
+                    break
+
+            if not is_found:
+                ans.append(-1)
+
+        return ans
