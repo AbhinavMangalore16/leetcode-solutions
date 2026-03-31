4cc680ec2e11fb4b711d1fc4d8d0a5ed72ec00ec
Abhinav Mangalore <101643788+AbhinavMangalore16@users.noreply.github.com>
2026-03-31 21:37:51 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.py
@@ -0,0 +1,13 @@
+# Last updated: 3/31/2026, 9:37:49 PM
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        cookies = 0
+        gp, sp = 0,0 
+        while gp<len(g) and sp<len(s):
+            if s[sp]>=g[gp]:
+                cookies+=1
+                gp+=1
+            sp+=1
+        return cookies
